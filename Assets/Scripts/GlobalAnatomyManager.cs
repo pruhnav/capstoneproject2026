b17398d47647cd6e0feeb1e05fdfceecc66790aa
pruhnav <pranavbalachanderofficial@gmail.com>
2026-02-12 17:38:28 -0500

    Revert "Scenes uploading"
    This reverts commit f7e87da5edc2c7cbe54b38d1146f9f77561bfa64.

--- Assets/Scripts/GlobalAnatomyManager.cs
+++ Assets/Scripts/GlobalAnatomyManager.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit; // This is the most important line!
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GlobalAnatomyManager : MonoBehaviour
 {
-    // The "SelectEnterEventArgs" tells Unity this is for VR clicking
+    // This specific "SelectEnterEventArgs" version is what the VR controller needs
     public void MakeGlow(SelectEnterEventArgs args)
     {
+        // This finds the specific pin the professor just clicked
         GameObject clickedObject = args.interactableObject.transform.gameObject;
 
         if (clickedObject != null)
@@ -13,7 +14,7 @@ public class GlobalAnatomyManager : MonoBehaviour
             var renderer = clickedObject.GetComponentInChildren<Renderer>();
             if (renderer != null)
             {
-                // Forces the pin to be seen through the hand model
+                // Forces it to stay visible through the anatomy
                 renderer.material.renderQueue = 4000;
             }
         }
