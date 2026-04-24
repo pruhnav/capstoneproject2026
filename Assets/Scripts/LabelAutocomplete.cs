95d92146f95dc83263e3c705b7c44731d682e75c
Lasya <lasyamadhuri3904@gmail.com>
2026-04-23 22:11:35 -0400

    suggestions selection

--- Assets/Scripts/LabelAutocomplete.cs
+++ Assets/Scripts/LabelAutocomplete.cs
@@ -81,7 +81,6 @@ public class LabelAutocomplete : MonoBehaviour
             GameObject btnObj = Instantiate(suggestionButtonPrefab, suggestionsContainer);
             spawnedButtons.Add(btnObj);
 
-            btnObj.AddComponent<TrackedDeviceGraphicRaycaster>();
             btnObj.GetComponentInChildren<TMP_Text>().text = word;
 
             Button btn = btnObj.GetComponent<Button>();
